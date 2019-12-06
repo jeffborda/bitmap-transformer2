f8f85d7f37370f9c5daa5ae9f0353f65be50cbfa
Jeff Borda <jeffborda@outlook.com>
2019-12-06 13:49:37 -0800

    able to run without cli args. taking in a file and saving it again.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /*
  *  CLI app that takes in a BMP image and does various transformations on the image.
  *  App takes in three arguments from the command line: input file path, output file path, and the desired transformation.
@@ -7,6 +9,17 @@
 public class App {
 
     public static void main(String[] args) {
+
+        try {
+
+            Bitmap myBitmap = new Bitmap("bmp-images/nice_day.bmp", "bmp-images/init_test.bmp");
+            myBitmap.save();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            System.out.println("Error reading file. " + e);
+        }
 
 
     }
