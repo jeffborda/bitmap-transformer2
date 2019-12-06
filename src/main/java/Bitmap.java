ebfde7d60c92fabb62517230c9e7b848be03c8f5
Jeff Borda <jeffborda@outlook.com>
2019-12-06 13:31:00 -0800

    created Bitmap class, with constructor, save method, getters

--- src/main/java/Bitmap.java
+++ src/main/java/Bitmap.java
@@ -1,2 +1,34 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Bitmap {
+
+    private Path bmpOutputPath;
+    private BufferedImage bmpData;
+
+    public Bitmap(String inputPath, String outputPath) throws IOException {
+
+        // RE: https://stackoverflow.com/questions/16919501/create-a-path-from-string-in-java7
+        Path bmpInputPath = Paths.get(inputPath);
+        this.bmpOutputPath = Paths.get(outputPath);
+        this.bmpData = ImageIO.read(bmpInputPath.toFile());
+    }
+
+    public void save() throws IOException {
+
+        // RE: https://blog.idrsolutions.com/2018/08/how-to-write-out-bmp-images-in-java/
+        ImageIO.write(bmpData, "bmp", bmpOutputPath.toFile());
+        System.out.println("Saved file to: " + bmpOutputPath);
+    }
+
+    public Path getBmpOutputPath() {
+        return bmpOutputPath;
+    }
+
+    public BufferedImage getBmpData() {
+        return bmpData;
+    }
 }
